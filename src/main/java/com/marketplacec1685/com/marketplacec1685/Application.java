343b6337f4f89f3804b0b7df9923d6e35c64b7d1
brunovt074 <brunodev.pdl@gmail.com>
2024-02-12 20:44:45 -0300

    project initialized

---
+++ src/main/java/com/marketplacec1685/com/marketplacec1685/Application.java
@@ -0,0 +1,13 @@
+package com.marketplacec1685.com.marketplacec1685;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
