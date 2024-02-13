7f116ded55e5a7239fcec3184cf55955d1a771e7
brunovt074 <brunodev.pdl@gmail.com>
2024-02-13 19:53:26 -0300

    test folder refactored

--- src/main/java/com/marketplace/Application.java
+++ src/main/java/com/marketplace/Application.java
@@ -1,4 +1,4 @@
-package com.marketplacec1685;
+package com.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/marketplace/ApplicationTests.java
+++ src/test/java/com/marketplace/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.marketplacec1685;
+package com.marketplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
