664059f865868a907288c8389cbe9fdbaa23b2dc
gonzamiguel <gonzalo.augustin.miguel@gmail.com>
2024-02-19 16:51:00 -0300

    Organizacion componentes

--- marketplace/src/App.jsx
+++ marketplace/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class=" text-5xl bg-black text-stone-50">
-          Hello world!
-      </h1>
+    <h1 className=' text-5xl bg-black text-red-600'>Hola Mundo </h1>
     </>
   )
 }
