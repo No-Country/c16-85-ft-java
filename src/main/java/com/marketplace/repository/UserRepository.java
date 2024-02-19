7c268f65cb98d7913d2e0a6ad49e3b68d1a6866e
brunovt074 <brunodev.pdl@gmail.com>
2024-02-19 02:16:30 -0300

    security.ApplicationConfig created and created and implemented UserDetailsService bean.

--- src/main/java/com/marketplace/repository/UserRepository.java
+++ src/main/java/com/marketplace/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.marketplace.repository;
 
 import com.marketplace.models.entity.UserAccount;
+import com.marketplace.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<UserAccount, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String username);
 }
