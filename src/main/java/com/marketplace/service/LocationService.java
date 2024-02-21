2d42b651c13e5d9eef2f3960bccd997f329ac06d
Jorge <jorgechasu@gmail.com>
2024-02-21 13:36:27 -0500

    metodos clase location

--- src/main/java/com/marketplace/service/LocationService.java
+++ src/main/java/com/marketplace/service/LocationService.java
@@ -1,4 +1,21 @@
 package com.marketplace.service;
 
+import com.marketplace.models.entity.Location;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface LocationService {
-}
+
+        List<Location> listLocations();
+
+        Location buscarPorId(UUID id);
+
+
+        Location guardarlocations(Location newlocation);
+
+        Location editarLocationPorID (UUID id, Location locationActulizada);
+
+        void locationborrar (UUID id);
+    }
+
