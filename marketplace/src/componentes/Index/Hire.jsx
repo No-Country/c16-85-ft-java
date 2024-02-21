8f0d7fd9144a73c50be8ad74f5e3b9aed5c4580f
Federico Levatti <federico.levatti@hotmail.com>
2024-02-21 17:03:32 -0300

    Componente Servicio inicial

--- marketplace/src/componentes/Index/Hire.jsx
+++ marketplace/src/componentes/Index/Hire.jsx
@@ -8,7 +8,7 @@ const SearchJob = () => {
      <>
        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="flex flex-col items-center justify-between lg:flex-row">
-        <div className="lg:max-w-lg lg:pr-5 lg:mb-0">
+        <div className="mb-2 lg:max-w-lg lg:pr-5 lg:mb-0">
           <div className="max-w-xl mb-2">
          
             <h2 className="max-w-lg mb-2 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none text-center">
@@ -23,7 +23,7 @@ const SearchJob = () => {
           <div className="flex flex-col items-center md:flex-row justify-center">
             <a
               href="/"
-              className="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide  transition duration-200 rounded shadow-md md:w-auto  md:mb-0 bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none bg-white  text-gray-800  py-2  border border-gray-400  "
+              className="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide  transition duration-200 rounded shadow-md md:w-auto  md:mb-0 bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none bg-white  text-gray-800    py-2  border border-gray-400  "
             >
               <span className="m-auto ">Ofrezca tus servicios</span>
             
@@ -59,7 +59,7 @@ const SearchService = () => {
           />
         
         </div>
-        <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0  lg:pl-10" >
+        <div className="mb-10 lg:max-w-lg lg:pr-5  3  lg:mb-0     lg:pl-10" >
           <div className="max-w-xl mb-2 pt-3">
             <h2 className="max-w-lg mb-2 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none text-center">
             Consiga el Servicio que Necesita en Nuestra Plataforma
--- marketplace/src/componentes/Index/Service.jsx
+++ marketplace/src/componentes/Index/Service.jsx
@@ -1,9 +1,42 @@
+
+
+const OptionTwo = () => {
+
+    return (<>
+
+  <div className="flex w-[90%] md:w-[80%] justify-center m-auto my-6 flex-wrap">
+  <div className="h-64 rounded-lg bg-gray-200 max-w-[40%] w-[100%]   min-w-[95%]  md:min-w-[60%]   lg:min-w-[300px]    ml-4  my-5 "></div>
+  <div className="h-64 rounded-lg bg-gray-200 max-w-[40%] w-[100%]   min-w-[95%]  md:min-w-[60%]  lg:min-w-[300px]    ml-4 my-5"></div>
+  <div className="h-64 rounded-lg bg-gray-200 max-w-[40%] w-[100%]   min-w-[95%]  md:min-w-[60%]  lg:min-w-[300px]    ml-4 my-5"></div>
+  <div className="h-64 rounded-lg bg-gray-200 max-w-[40%] w-[100%]   min-w-[95%]  md:min-w-[60%]  lg:min-w-[300px]    ml-4 my-5"></div>
+</div>
+    </>)
+}
+
 const Service = () => {
+
+   const serviceName = ["Servicio 1" , "Servicio 2" , "Servicio 3" , "Servicio 4"]
+
+
     return (<>
 
 
+    <div >
+    <h1 className="text-center text-3xl font-semibold">Servicios</h1>
+     {/* <div className="flex flex-wrap justify-center "> */}
+        {/* {
+            serviceName.map(service => <CardOne/>) 
+        } */}
+   
+      
+     {/* </div> */}
+  
+      
 
-    <h1>Servicios</h1>
+
+     <OptionTwo/>
+    </div>
+
 
     
     </>)
