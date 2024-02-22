997a76adb900b1cd4052d3473c798f273ed84cb4
gonzamiguel <gonzalo.augustin.miguel@gmail.com>
2024-02-22 18:02:14 -0300

    Pruebas Login

--- marketplace/src/App.jsx
+++ marketplace/src/App.jsx
@@ -4,18 +4,20 @@ import Navbar from "./componentes/Navbar/Navbar";
 import Footer from "./componentes/Footer/Footer";
 import Index from './pages';
 import Login from "./componentes/User/Login";
+import Register from './componentes/User/Register';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/Registrarse" element={<Login />} />
+        <Route path="/Registrarse" element={<Login/>} />
+        <Route path="/Prueba" element={<Register/>} />
       </Routes>
     </Router>
   );
 }
-
 
 
 function Home() {
