f82ad0f532e22b9aaa717dcc28c6dc7c29af9a80
maty-carrion <mcsadevp@gmail.com>
2024-02-23 11:19:18 -0300

    Creacion Script SQL (data.sql que pobla la base de datos por medio de Querys proyecto con datos del dia 23-02-2024 se puede utilizar como plantilla para probar antes de merggear con Dev)

--- src/main/java/com/marketplace/models/entity/Location.java
+++ src/main/java/com/marketplace/models/entity/Location.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.UUID;
+
 
 @Entity(name = "locations")
 @Getter
@@ -30,7 +30,5 @@ public class Location {
 
     @OneToOne
     private ContractorProfile contractorProfile;
-
-
 
 }
--- src/main/java/com/marketplace/models/valueobjets/addres/Addres.java
+++ src/main/java/com/marketplace/models/valueobjets/addres/Addres.java
@@ -2,6 +2,13 @@ package com.marketplace.models.valueobjets.addres;
 
 import jakarta.persistence.Embeddable;
 
+import java.util.Objects;
+
 @Embeddable
-public record Addres (){
+public record Addres (String comuna,String direccion,String numero){
+    public  Addres{
+        Objects.requireNonNull(comuna, "Comuna cannot be null");
+        Objects.requireNonNull(direccion, "Direccion cannot be null");
+        Objects.requireNonNull(numero, "Numero cannot be null");
+    }
 }
