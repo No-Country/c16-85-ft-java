d84b0fb1812825ff7e49269a2116b748c9fafaff
Rocha Nahuel <rocha.nahuel@hotmail.com>
2024-02-23 22:38:05 -0300

    act contractor

---
+++ src/main/java/com/marketplace/models/mapper/contractor/ContractorProfileMapper.java
@@ -0,0 +1,14 @@
+package com.marketplace.models.mapper.contractor;
+
+import com.marketplace.DTO.contractor.ContractorProfileResponse;
+import com.marketplace.models.entity.ContractorProfile;
+
+public class ContractorProfileMapper {
+    public ContractorProfileResponse mapContractorToResponse(ContractorProfile contractor) {
+        return new ContractorProfileResponse(
+                contractor.getBusinessName().businessName(),
+                contractor.getCeoName().name(),
+                contractor.getCeoLastName().lastName()
+        );
+    }
+}
