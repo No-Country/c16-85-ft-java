77aa8228f9d484419104b8f83eabc1aab2883f78
brunovt074 <brunodev.pdl@gmail.com>
2024-03-05 22:25:58 -0300

    delete security.demo package

--- src/main/java/com/marketplace/security/userauth/controller/AdminController.java
+++ src/main/java/com/marketplace/security/userauth/controller/AdminController.java
@@ -1,4 +1,4 @@
-package com.marketplace.security.demo.controller;
+package com.marketplace.security.userauth.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
